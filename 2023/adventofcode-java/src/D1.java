8c0ee02a2ef7dff17276558161cdd9b3ff3ae769
Matei <mateisr@proton.me>
2023-12-04 21:45:34 +0200

    Day 1 Java 2023

--- 2023/adventofcode-java/src/D1.java
+++ 2023/adventofcode-java/src/D1.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+import java.io.File;
 public class D1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            String line = s.nextLine();
+            if (line.isEmpty() || line.isBlank()) break;
+            int firstNum = -1, lastNum = 0;
+            for (char ch: line.toCharArray()) {
+                if (Character.isDigit(ch)) {
+                    if (firstNum == -1) firstNum = ch - '0';
+                    lastNum = ch - '0';
+                }
+            }
+            int completeNum = firstNum * 10 + lastNum;
+            sum += completeNum;
+            System.out.println(completeNum);
+        }
+        System.out.printf("\n%d", sum);
+    }
 }
